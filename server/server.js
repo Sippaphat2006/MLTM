71f84816bd00844296f756406290b9b5c1839c18
Num Nim <65991024@kmitl.ac.th>
2025-10-03 14:41:07 +0700

    feat: update server.js, remove db_connection, edit note.txt

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('MLTM server is up'));
 app.use('/api', serverRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // // only listen after DB is reachable
